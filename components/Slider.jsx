ae500659e89b5dd46c00fa499d0c2753a0f0f99b
ismetcahangirov <smitcrown4@gmail.com>
2025-10-25 13:35:42 +0400

    slider update

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -7,15 +7,14 @@ const Slider = ({ children, className }) => {
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
+
   const onMouseDown = (e) => {
     setIsDragging(true);
     setStartX(e.pageX - sliderRef.current.offsetLeft);
     setScrollLeft(sliderRef.current.scrollLeft);
   };
-
   const onMouseLeave = () => setIsDragging(false);
   const onMouseUp = () => setIsDragging(false);
-
   const onMouseMove = (e) => {
     if (!isDragging) return;
     e.preventDefault();
@@ -23,6 +22,20 @@ const Slider = ({ children, className }) => {
     const walk = x - startX;
     sliderRef.current.scrollLeft = scrollLeft - walk;
   };
+
+  const onTouchStart = (e) => {
+    setIsDragging(true);
+    setStartX(e.touches[0].pageX - sliderRef.current.offsetLeft);
+    setScrollLeft(sliderRef.current.scrollLeft);
+  };
+  const onTouchMove = (e) => {
+    if (!isDragging) return;
+    const x = e.touches[0].pageX - sliderRef.current.offsetLeft;
+    const walk = x - startX;
+    sliderRef.current.scrollLeft = scrollLeft - walk;
+  };
+  const onTouchEnd = () => setIsDragging(false);
+
   return (
     <div
       ref={sliderRef}
@@ -31,6 +44,9 @@ const Slider = ({ children, className }) => {
       onMouseLeave={onMouseLeave}
       onMouseUp={onMouseUp}
       onMouseMove={onMouseMove}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
     >
       {children}
     </div>
